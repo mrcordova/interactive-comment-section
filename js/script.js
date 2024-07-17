0c5b5b7abd88295f02014445aec17e38dfd46627
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-07-17 16:15:40 -0700

    make create function for svg

--- js/script.js
+++ js/script.js
@@ -21,7 +21,17 @@ function openDialog() {
 function closeDialog() {
   dialog.close();
 }
+const createSvg = function (width, height, d, fill) {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttributeNS(null, "width", width);
+  svg.setAttributeNS(null, "height", height);
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttributeNS(null, "d", d);
+  path.setAttributeNS(null, "fill", fill);
 
+  svg.appendChild(path);
+  return svg;
+};
 const createComment = function () {
   const user = "amyrobson";
   const commentDiv = document.createElement("div");
@@ -78,23 +88,29 @@ const createComment = function () {
   commentDiv.appendChild(scoreDiv);
 
   const plusBtn = document.createElement("button");
-  plusBtn.classList.add("plus-sign-btn");
-  const plusSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  // plusSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  plusSvg.setAttributeNS(null, "width", 11);
-  plusSvg.setAttributeNS(null, "height", 11);
-  const plusPath = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "path"
+  // plusBtn.classList.add("plus-sign-btn");
+  // const plusSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  // plusSvg.setAttributeNS(null, "width", 11);
+  // plusSvg.setAttributeNS(null, "height", 11);
+  // const plusPath = document.createElementNS(
+  //   "http://www.w3.org/2000/svg",
+  //   "path"
+  // );
+  // plusPath.setAttributeNS(
+  //   null,
+  //   "d",
+  //   "M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
+  // );
+  // plusPath.setAttributeNS(null, "fill", "#C5C6EF");
+  // plusSvg.appendChild(plusPath);
+  plusBtn.appendChild(
+    createSvg(
+      11,
+      11,
+      "M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z",
+      "#C5C6EF"
+    )
   );
-  plusPath.setAttributeNS(
-    null,
-    "d",
-    "M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
-  );
-  plusPath.setAttributeNS(null, "fill", "#C5C6EF");
-  plusSvg.appendChild(plusPath);
-  plusBtn.appendChild(plusSvg);
 
   scoreDiv.appendChild(plusBtn);
 
@@ -105,53 +121,87 @@ const createComment = function () {
 
   const minusBtn = document.createElement("button");
   minusBtn.classList.add("minus-sign-btn");
-  const minusSvg = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "svg"
+  // const minusSvg = document.createElementNS(
+  //   "http://www.w3.org/2000/svg",
+  //   "svg"
+  // );
+  // minusSvg.setAttributeNS(null, "width", 11);
+  // minusSvg.setAttributeNS(null, "height", 11);
+  // const minusPath = document.createElementNS(
+  //   "http://www.w3.org/2000/svg",
+  //   "path"
+  // );
+  // minusPath.setAttributeNS(
+  //   null,
+  //   "d",
+  //   "M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
+  // );
+  // minusPath.setAttributeNS(null, "fill", "#C5C6EF");
+  // minusSvg.appendChild(minusPath);
+  minusBtn.appendChild(
+    createSvg(
+      11,
+      11,
+      "M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z",
+      "#C5C6EF"
+    )
   );
-  minusSvg.setAttributeNS(null, "width", 11);
-  minusSvg.setAttributeNS(null, "height", 11);
-  const minusPath = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "path"
-  );
-  minusPath.setAttributeNS(
-    null,
-    "d",
-    "M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
-  );
-  minusPath.setAttributeNS(null, "fill", "#C5C6EF");
-  minusSvg.appendChild(minusPath);
-  minusBtn.appendChild(minusSvg);
 
   scoreDiv.appendChild(minusBtn);
 
   // check if current user
-  const replyBtn = document.createElement("button");
-  replyBtn.setAttribute("class", "reply-container rubik-700");
+  // const replyBtn = document.createElement("button");
+  // replyBtn.setAttribute("class", "reply-container rubik-700");
 
-  const replySvg = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "svg"
-  );
-  replySvg.setAttributeNS(null, "width", 14);
-  replySvg.setAttributeNS(null, "height", 13);
-  const replyPath = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "path"
-  );
-  replyPath.setAttributeNS(
-    null,
-    "d",
-    " M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z"
-  );
-  replyPath.setAttributeNS(null, "fill", "#5357B6");
-  replySvg.appendChild(replyPath);
-  replyBtn.appendChild(replySvg);
-  const replyText = document.createTextNode("Reply");
-  replyBtn.appendChild(replyText);
+  // replyBtn.appendChild(
+  //   createSvg(
+  //     14,
+  //     13,
+  //     "M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z",
+  //     "#5357B6"
+  //   )
+  // );
+  // const replyText = document.createTextNode("Reply");
 
-  commentDiv.appendChild(replyBtn);
+  // replyBtn.appendChild(replyText);
+  // commentDiv.appendChild(replyBtn);
+
+  const currentUserBtns = document.createElement("div");
+  currentUserBtns.classList.add("current-user-btns");
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("class", "delete-btn rubik-700");
+
+  deleteBtn.appendChild(
+    createSvg(
+      12,
+      14,
+      "M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z",
+      "#ED6368"
+    )
+  );
+  const deleteText = document.createTextNode("Delete");
+  deleteBtn.appendChild(deleteText);
+
+  const editBtn = document.createElement("button");
+  editBtn.setAttribute("class", "edit-btn rubik-700");
+
+  editBtn.appendChild(
+    createSvg(
+      14,
+      14,
+      "M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z",
+      "#5357B6"
+    )
+  );
+
+  const editText = document.createTextNode("Edit");
+  editBtn.appendChild(editText);
+
+  currentUserBtns.appendChild(deleteBtn);
+  currentUserBtns.appendChild(editBtn);
+
+  commentDiv.appendChild(currentUserBtns);
 
   comments.appendChild(commentDiv);
 };
